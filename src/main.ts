740ac408942637ed6899158b3ade4cca99e3b4ee
Bram Borggreve <borggreve@gmail.com>
2021-02-22 04:59:17 -0500

    feat: implement basic Kin transaction example

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,52 @@
-import { randomId, sleep } from './utils'
+import { Environment, quarksToKin, TransactionType } from '@kinecosystem/kin-sdk-v2'
+import { Kin } from './kin'
+import { sleep } from './utils'
 
 export async function main(): Promise<number> {
-  const rand = randomId()
-  await Promise.all([
-    sleep(1000).then(() => console.log(rand + ' I slept for 1000 ms')),
-    sleep(2000).then(() => console.log(rand + ' I slept for 2000 ms')),
-    sleep(1500).then(() => console.log(rand + ' I slept for 1500 ms')),
-    sleep(500).then(() => console.log(rand + ' I slept for 500 ms')),
-  ])
+  const kin = new Kin(Environment.Test)
+
+  const alicePrivateKey = Kin.generateKey()
+  console.log(`Public Key Alice    ${alicePrivateKey.publicKey().toBase58()}`)
+
+  const aliceTokenAccounts = await kin.createAccount(alicePrivateKey)
+
+  for (const tokenAccount of aliceTokenAccounts) {
+    console.log(`Token Account Alice ${tokenAccount.toBase58()}`)
+  }
+
+  const bobPrivateKey = Kin.generateKey()
+  console.log(`Public Key Bob      ${bobPrivateKey.publicKey().toBase58()}`)
+  const bobTokenAccounts = await kin.createAccount(bobPrivateKey)
+
+  for (const tokenAccount of bobTokenAccounts) {
+    console.log(`Token Account Bob   ${tokenAccount.toBase58()}`)
+  }
+
+  async function sleepAndPrintBalances() {
+    console.log('Sleeping for a bit...')
+    await sleep(15)
+    await kin.getBalance(alicePrivateKey.publicKey()).then((b) => {
+      console.log(`Balance for Alice:  ${quarksToKin(b)} Kin`)
+    })
+    await kin.getBalance(bobPrivateKey.publicKey()).then((b) => {
+      console.log(`Balance for Bob:    ${quarksToKin(b)} Kin`)
+    })
+  }
+
+  await sleepAndPrintBalances()
+
+  console.log('Request Airdrop for Alice')
+  await kin.requestAirdrop(aliceTokenAccounts[0], '10')
+
+  console.log('Request Airdrop for Bob')
+  await kin.requestAirdrop(bobTokenAccounts[0], '10')
+
+  await sleepAndPrintBalances()
+
+  console.log('Submitting Payment from Alice to Bob')
+  await kin.submitPayment(alicePrivateKey, bobPrivateKey.publicKey(), '2', TransactionType.Earn, 'My demo payment')
+
+  await sleepAndPrintBalances()
 
   console.log('Done!')
   return 0
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
-export const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms))
+export const sleep = (seconds = 1) => new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 export const randomId = (size = 5) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('')
